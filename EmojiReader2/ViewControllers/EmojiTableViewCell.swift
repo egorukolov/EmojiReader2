16c69c6afca665ae84e53965a6ea469b1e7ff9ea
Egor Ukolov <ukolovegor@yandex.ru>
2024-06-01 11:05:20 +0700

    Реализация модели, настройка ячейки

--- EmojiReader2/ViewControllers/EmojiTableViewCell.swift
+++ EmojiReader2/ViewControllers/EmojiTableViewCell.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class EmojiTableViewCell: UITableViewCell {
-
+    @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-
+    func set (object: Emoji) {
+        
+        self.emojiLabel.text = object.emoji
+        self.nameLabel.text = object.name
+        self.descriptionLabel.text = object.description
+        
+    }
+    
 }
