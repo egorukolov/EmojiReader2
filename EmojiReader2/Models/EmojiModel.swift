719d51ea10709f903150691a4663ffba1b0f58e3
Egor Ukolov <ukolovegor@yandex.ru>
2024-06-01 10:43:21 +0700

    Начальные настройки таблицы, создание модели

---
+++ EmojiReader2/Models/EmojiModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiModel.swift
+//  EmojiReader2
+//
+//  Created by Egor Ukolov on 01.06.2024.
+//
+
+import Foundation
---
+++ EmojiReader2/ViewControllers/EmojiTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader2
+//
+//  Created by Egor Ukolov on 30.05.2024.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
